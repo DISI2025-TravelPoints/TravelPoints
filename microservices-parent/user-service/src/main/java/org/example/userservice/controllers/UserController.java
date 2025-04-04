dd6cbdd9bc1cb2a4babc00add1c801f3e40403e4
foctavian <tavi.latavi121@gmail.com>
2025-04-04 23:47:05 +0300

    created reverse proxy

--- microservices-parent/user-service/src/main/java/org/example/userservice/controllers/UserController.java
+++ microservices-parent/user-service/src/main/java/org/example/userservice/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.example.userservice.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
     @RequestMapping("/")
     public String hello() {
