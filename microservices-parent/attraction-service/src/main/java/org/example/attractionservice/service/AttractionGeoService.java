e8eb8ecd60a290d1881e27ff144c0361e86cafe0
foctavian <tavi.latavi121@gmail.com>
2025-04-24 01:25:34 +0300

    fixed s3 file deletion

---
+++ microservices-parent/attraction-service/src/main/java/org/example/attractionservice/service/AttractionGeoService.java
@@ -0,0 +1,39 @@
+package org.example.attractionservice.service;
+
+import lombok.AllArgsConstructor;
+import org.example.attractionservice.mapper.entity.AttractionDocument;
+import org.example.attractionservice.repository.AttractionGeoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AttractionGeoService {
+    private final AttractionGeoRepository attractionGeoRepository;
+
+
+    public void saveLocation(UUID attractionId, Float latitude, Float longitude) {
+        GeoJsonPoint location = new GeoJsonPoint(Double.valueOf(longitude), Double.valueOf(latitude));
+        attractionGeoRepository.save(new AttractionDocument(attractionId, location));
+    }
+
+    public List<AttractionDocument> getAllLocations() {
+        return attractionGeoRepository.findAll();
+    }
+
+    public AttractionDocument getLocationById(UUID attractionId) {
+        return attractionGeoRepository.findById(attractionId).orElse(null);
+    }
+
+    public void deleteLocationById(UUID attractionId) {
+        attractionGeoRepository.deleteById(attractionId);
+    }
+
+    public boolean exists(UUID attractionId) {
+        return attractionGeoRepository.existsById(attractionId);
+    }
+}
--- microservices-parent/attraction-service/src/main/java/org/example/attractionservice/service/AttractionService.java
+++ microservices-parent/attraction-service/src/main/java/org/example/attractionservice/service/AttractionService.java
@@ -1,20 +1,73 @@
 package org.example.attractionservice.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.example.attractionservice.mapper.dto.AttractionRequest;
+import org.example.attractionservice.mapper.dto.AttractionGetRequest;
+import org.example.attractionservice.mapper.dto.AttractionPostRequest;
+import org.example.attractionservice.mapper.entity.Attraction;
 import org.example.attractionservice.repository.AttractionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
+import java.sql.Date;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AttractionService {
     @Autowired
     private final AttractionRepository attractionRepository;
 
-//    public UUID createAttraction(AttractionRequest attractionRequest){
-//
-//    }
+    public UUID createAttraction(AttractionPostRequest attractionPostRequest, String filename) {
+        Attraction attraction = Attraction.builder()
+                .name(attractionPostRequest.getName())
+                .description(attractionPostRequest.getDescription())
+                .entryFee(attractionPostRequest.getEntryFee())
+                .audioFilePath(filename)
+                .lastUpdate(new Date(System.currentTimeMillis()))
+                .build();
+        return attractionRepository.save(attraction).getId();
+    }
+
+    public List<AttractionGetRequest> getAllAttractions() {
+        List<Attraction> attractions = attractionRepository.findAll();
+
+        return attractions.stream().map(this::mapAttractionToRequest).collect(Collectors.toList());
+    }
+
+    private AttractionGetRequest mapAttractionToRequest(Attraction attraction){
+        return AttractionGetRequest.builder()
+                .id(attraction.getId())
+                .audioFilePath(attraction.getAudioFilePath())
+                .description(attraction.getDescription())
+                .name(attraction.getName())
+                .entryFee(attraction.getEntryFee())
+                .lastUpdate(attraction.getLastUpdate())
+                .build();
+    }
+
+
+    public AttractionGetRequest getAttractionById(UUID attractionId) {
+        Attraction attraction = attractionRepository.findById(attractionId).orElse(null);
+        if(attraction != null){
+            return mapAttractionToRequest(attraction);
+        }
+        return null;
+    }
+
+    public Boolean deleteAttractionById(UUID attractionId) {
+        if (attractionRepository.existsById(attractionId)) {
+            attractionRepository.deleteById(attractionId);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean exists(UUID attractionId) {
+        return attractionRepository.existsById(attractionId);
+    }
 }
