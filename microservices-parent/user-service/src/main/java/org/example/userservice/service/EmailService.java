25679808453450642475d8fcc3efd71c03423287
antoniacociubei <antonia.cociubei@gmail.com>
2025-04-27 18:49:41 +0300

    added deploy link

--- microservices-parent/user-service/src/main/java/org/example/userservice/service/EmailService.java
+++ microservices-parent/user-service/src/main/java/org/example/userservice/service/EmailService.java
@@ -5,7 +5,6 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-
 import java.io.UnsupportedEncodingException;
 
 @Service
@@ -19,6 +18,7 @@ public class EmailService {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
+            //String resetLink = "http://travelpoints.run.place/reset-pass?token=" + token;
             String resetLink = "http://localhost:3000/reset-pass?token=" + token;
             String htmlMsg = "<p>Hi there!</p>" +
                     "<p>We received a request to reset your password for your TravelPoints account.</p>" +
