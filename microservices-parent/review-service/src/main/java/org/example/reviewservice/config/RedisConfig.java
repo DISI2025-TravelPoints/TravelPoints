6401a0b0d1e4515c771b55814e2c744ff7b0721f
biancachitu <bianca.ioana.chitu@gmail.com>
2025-05-19 15:56:08 +0300

    fixed redis config

--- microservices-parent/review-service/src/main/java/org/example/reviewservice/config/RedisConfig.java
+++ microservices-parent/review-service/src/main/java/org/example/reviewservice/config/RedisConfig.java
@@ -4,13 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String hostName;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(hostName);
+        redisConfig.setPort(port);
+        return new LettuceConnectionFactory(redisConfig);
     }
 
     @Bean
